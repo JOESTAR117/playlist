1b2f40990b355401232705c25ef394c7d3520fba
Jackson Rios <juniorrios1231@gmail.com>
2023-01-04 19:29:01 -0300

    edit music

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Music = require("./model/music");
 
 const app = express();
 const port = process.env.PORT || 3000;
+let music = null;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -20,7 +21,7 @@ app.get("/", async (req, res) => {
 
 app.get("/admin", async (req, res) => {
   const playlist = await Music.find();
-  await res.render("admin",{playlist});
+  await res.render("admin", { playlist, music: null });
 });
 
 app.post("/create", async (req, res) => {
@@ -28,6 +29,20 @@ app.post("/create", async (req, res) => {
   await Music.create(music);
   res.redirect("/");
 });
+
+app.get("/by/:id", async (req, res) => {
+  const { id } = req.params;
+  music = await Music.findById({ _id: id });
+  const playlist = await Music.find();
+  res.render("admin", { playlist, music });
+});
+
+app.post("/uptade/:id",async(req,res) =>{
+  const newMusic = req.body
+  await Music.updateOne({_id: req.params.id},newMusic)
+  res.redirect("/admin")
+}) 
+
 
 app.listen(port, () => {
   console.log(`Server listening on port http://localhost:${port} `);
